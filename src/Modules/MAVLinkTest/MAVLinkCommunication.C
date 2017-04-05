eb6f976a1e986d4cd3a1374a1ed443df352a6f46
alsaibie <ali@alsaibie.com>
2017-04-04 22:42:42 -0400

    Some more random changes on MAVLink

--- src/Modules/MAVLinkTest/MAVLinkCommunication.C
+++ src/Modules/MAVLinkTest/MAVLinkCommunication.C
@@ -17,11 +17,12 @@ jevois::Component(instance)
   jevois::engine::serialdev =""; 
 	
   // Add Serial locally here 
-  itsSerial = addSubComponent<jevois::Serial>("serial", jevois::UserInterface::Type::Hard);
-  itsSerial->setParamVal("devname", MAVLinkCommunication::serialdev);
+  itsSerial = addSubComponent<jevois::Serial>("serial", MAVLinkCommunication::UserInterface::Type::Hard);
+  itsSerial-> setParamVal("devname", MAVLinkCommunication::serialdev); 
+  // Or?  What is MAVLinkCommunication::serialdev type? 
+  // itsSerial-> setParamString("devname", MAVLinkCommunication::serialdev); 
 
-
-	// Reset MAVLink Parameters to Default
+	// Reset MAVLink Parameters to Default (includes setting System ID etc)
 	MAVLink_data_reset_param_defaults();
 }
 
@@ -34,7 +35,7 @@ MAVLinkCommunication::~MAVLinkCommunication()
 
 // ####################################################################################################
 void MAVLinkCommunication::sendSystemState(void){
-
+  mavlink_msg_heartbeat_send(MAVLINK_COMM_0, MAVData->param[PARAM_SYSTEM_TYPE], MAVData->param[PARAM_AUTOPILOT_TYPE], 0, 0, 0);
 }
 
 // ####################################################################################################
@@ -44,6 +45,22 @@ void MAVLinkCommunication::sendParameters(void){
 
 // ####################################################################################################
 void MAVLinkCommunication::receive(void){
+  mavlink_message_t msg;
+  mavlink_status_t status = { 0 };
+
+  //This is blocking, need to change, but general idea is
+  while (charavailable)
+  {
+    uint8_t c = itsSerial->read(MAVLink_buffer, 1);
+
+    /* Try to get a new message */
+    if (mavlink_parse_char(MAVLINK_COMM_0, c, &msg, &status))
+    {
+      /* Handle message */
+      handle_mavlink_message(&msg);
+    }
+    /* And get the next one */
+  }
 
 }
 
--- src/Modules/MAVLinkTest/MAVLinkTest.C
+++ src/Modules/MAVLinkTest/MAVLinkTest.C
@@ -5,15 +5,20 @@
     \ingroup modules */
 
 #include <jevois/Core/Module.H>
+#include "MAVLinkCommunication.h"
+#include "MAVLinkTest_settings.h"
 
-#include "settings.h"
+namespace MAVLinkn {
+
+
+}
 
 class MAVLinkTest : public jevois::Module
 {
   public:
     //! Default base class constructor ok
     MAVLinkTest(std::string const & instance) : jevois::Module(instance)
-    { itsMAVLink = addSubComponent<MAVLinkCOmmunication>("MAVLink communication"); }
+    { itsMAVLink = addSubComponent<MAVLinkCommunication>("MAVLink communication", &MAVLink_data); }
 
     //! Virtual destructor for safe inheritance
     virtual ~MAVLinkTest() { }
@@ -21,17 +26,31 @@ class MAVLinkTest : public jevois::Module
     //! Processing function
     virtual void process(jevois::InputFrame && inframe, jevois::OutputFrame && outframe) override
     {
+        // Send Heartbeat Every 1 second
+        itsMAVLink->senSystemState();
+
+
+        // Read Periodically
+        itsMAVLink->receive();
+
+        // Send Parameters Periodically
+        itsMAVLink->sendParameters();
 
     }
+    
+    mavlink_attitude_t attitude;
 
-    virtual void sendSerial(std::string const & str) override
-    {
-
-    }
-    virtual void parseSerial(std::string const & str, std::shared_ptr<UserInterface> s){
-      
-    }
+protected:
+    jevois::Timer itsProcessingTimer;
+    std::shared_ptr<MAVLinkCommunication> itsMAVLink;
 };
 
+//! Define handle_mavlink_message here
+MAVLinkCommunication::handle_mavlink_message(mavlink_message_t* msg){
+
+//Somewhere in here
+    case MAVLINK_MSG_ID_ATTITUDE:
+    mavlink_msg_attitude_decode(msg, & MAVLinkTest::attitude);
+}
 // Allow the module to be loaded as a shared object (.so) file:
 JEVOIS_REGISTER_MODULE(MAVLinkTest);
